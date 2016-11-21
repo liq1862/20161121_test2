cda8e92115be72fe6e09d05b4c395776a7a3b22f
lili <liq1862@gmail.com>
2016-11-21 14:25:09 +0800

    需加入user...

---
+++ app/src/main/java/com/example/user/a20161121_test2/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.user.a20161121_test2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                InputStream inputStream = null;
+
+                String str_url = "http://udn.com/rssfeed/news/1";
+                URL url = null;
+                try {
+                    url = new URL(str_url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    inputStream = conn.getInputStream();
+
+                    //==============
+                    ByteArrayOutputStream result = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) != -1) {
+                        result.write(buffer, 0, length);
+                    }
+                    String str = result.toString();
+                    Log.d("NET", str);
+                    MyDataHandler dataHandler = new MyDataHandler();
+                    SAXParserFactory spf = SAXParserFactory.newInstance();
+                    SAXParser sp = spf.newSAXParser();
+                    XMLReader xr = sp.getXMLReader();
+                    xr.setContentHandler(dataHandler);
+                    xr.parse(new InputSource(new StringReader(str)));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }.start();
+    }
+}
---
+++ app/src/main/java/com/example/user/a20161121_test2/MyDataHandler.java
@@ -0,0 +1,43 @@
+package com.example.user.a20161121_test2;
+
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created by user on 2016/11/21.
+ */
+
+public class MyDataHandler extends DefaultHandler {
+    boolean isTitle = false;
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (qName.equals("title"))
+        {
+            isTitle = true;
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+        if (qName.equals("title"))
+        {
+            isTitle = false;
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+        if (isTitle)
+        {
+            String str = new String(ch).substring(start, start + length);
+            Log.d("XML", str);
+        }
+
+    }
+}
